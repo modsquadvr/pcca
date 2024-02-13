47b859b4bc439ca8e2533181b44ab9847f68684c
Andy W <andy.wynden@gmail.com>
2024-02-12 19:50:02 -0800

    removing cruft

--- src/Router.js
+++ src/Router.js
@@ -13,22 +13,6 @@ export default function App() {
   return (
     <Router>
       <div>
-        {/* <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/heart">Heart</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>            
-          </ul>
-        </nav> */}
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
