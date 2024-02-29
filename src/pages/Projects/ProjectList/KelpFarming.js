dae96ac26e7b94b815efd0437b0e02f68657177d
Andy W <andy.wynden@gmail.com>
2024-02-28 17:52:40 -0800

    fixing photo names and adding photos to kelp farming card

--- src/pages/Projects/ProjectList/KelpFarming.js
+++ src/pages/Projects/ProjectList/KelpFarming.js
@@ -1,8 +1,18 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
 
 // eslint-disable-next-line
 import { FadeRuleLight, FadeRuleDark } from "components/FadeRule";
+import GridPhoto from "components/GridPhoto";
+
+import derekAndy from "assets/photos/Kelp-Farming/derek_andy_gimbal.jpg"
+import aldynView from "assets/photos/Kelp-Farming/aldyn_view.jpg"
+import group from "assets/photos/Kelp-Farming/group.jpg"
+import harvesting from "assets/photos/Kelp-Farming/harvesting.jpg"
+import lecture from "assets/photos/Kelp-Farming/nick_jasmine_lecture.jpg"
 
 
 export default function KelpFarming({project, ...props}) {
@@ -14,18 +24,45 @@ export default function KelpFarming({project, ...props}) {
         Kelp Farming
       </Typography>
       <FadeRuleDark marginBottom="2em"/>
-      <Typography paragraph>
-        The Minera Lead Mines were a mining operation and are now a country park and tourist centre in the village of Minera near Wrexham, in Wrexham County Borough, Wales.
-      </Typography>
-      <Typography paragraph>
-        The first written record of lead mining at Minera dates back to 1296, when Edward I of England hired miners from the site to work in his new mines in Devon. Not all of them vacated the area, however, as mining went on until the Black Death in 1349, when it ended.
-      </Typography>
-      <Typography paragraph>
-        In 1527, two men bought the rights to mine on the site, but deeper workings were unworkable due to the presence of underground rivers, and the inability to prevent flooding. The inability to pay for steam engines to pump out water closed the mines again until 1845, when John Taylor & Sons, mining agents from Flintshire, formed the Minera Mining Company. They were able to build a stationary steam engine on site, and also blast caves from down in the valley into the mines, for extra drainage. The steam engine was a Cornish engine (i.e. a Beam engine), typical for stationary engines at the time.
-      </Typography>
-      <Typography paragraph>
-        John Taylor & Sons had used a £30,000 investment at the time, yet the profits for 1864 alone were £60,000 (equivalent to over £4 Million in 2008. By 1900, the price of lead and zinc had fallen dramatically, while the price of coal used for the steam engine rose. The stationary steam engine stopped work in 1909. The owners sold off the mines and all assets by 1914. 
-      </Typography>
+      <Grid container spacing={4} justifyContent="center">
+
+        <Grid item xs={12}>
+          <Container component="img" src={harvesting} sx={{maxHeight:"600px", width:"auto"}}/>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Typography paragraph textAlign="left">
+            Calosoma retusum reaches about 25–32 millimetres (0.98–1.26 in) in length. This species usually has a bright metallic dark green or bronze green coloration, sometime with bluish reflections. The borders of the pronotum are rounded and raised. Elytra are striated, with large punctures. These beetles are voracious consumer of caterpillars, so they are considered beneficial insects for the agriculture.
+          </Typography>
+        </Grid>
+
+        <GridPhoto size="4" photo={lecture}/>
+        <GridPhoto size="4" photo={derekAndy}/>
+        <GridPhoto size="4" photo={aldynView}/>
+
+        <Grid item xs={12}>
+          <Typography paragraph textAlign="left">
+            There are only around 2000 types of beetles known to live in off-world colonies. The reasons why so few have managed to make the adaptations to space travel and other gravitational fields is poorly understood, but is thought to be related to the surface tension of water and its impacts on how exoskeletons have evolved to keep water contained within the creature. The beetle which seems to have made the most successful adaptation to life off-world is the Lunar Must Beetle[2], commonly known by Lunar residents as "heebie-jeebies", and which tends to colonize any relatively undisturbed fibers and textiles, such as disused carpeting or clothing. 
+          </Typography>
+        </Grid>
+
+
+        <Grid item xs={12}>
+          <Typography paragraph textAlign="left">
+            Why Orlando still hasn't written back is beyond me. The pigs won't enjoy much of the ritual, I am sure of it. So far they've been quick to find any possible escape route they can, in order to avoid being eaten. Perhaps Orlando has done the same? I've still got his pair of Sunday Corduroy Slacks, and cannot imagine him leaving them behind forever. 
+          </Typography>
+        </Grid>
+
+
+        <Grid item xs={12} md={6}>
+          <Typography component="h5" variant="h5" textAlign="center">
+            <Link href="https://im4lab.com/virtualproduction" target="_blank" rel="noopener noreferrer" >Visit the Project Website</Link>
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Container component="img" src={group} />
+        </Grid>
+      </Grid>
     </>
   );
 }
